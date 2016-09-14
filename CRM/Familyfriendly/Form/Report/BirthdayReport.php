ae646c269b831f75b2b206198383efba32f28bd7
Sarah Gladstone <sarah@fountaintribe.com>
2016-09-14 09:51:19 -0400

    updated membership filters

--- CRM/Familyfriendly/Form/Report/BirthdayReport.php
+++ CRM/Familyfriendly/Form/Report/BirthdayReport.php
@@ -22,47 +22,13 @@ class CRM_Familyfriendly_Form_Report_BirthdayReport extends CRM_Report_Form {
 		}
 	
 		//
-		$cur_domain_id = "-1";
-			
-		$result = civicrm_api3('Domain', 'get', array(
-				'sequential' => 1,
-				'current_domain' => "",
-		));
-			
-		if( $result['is_error'] == 0 ){
-			$cur_domain_id = $result['id'];
-		
-		}
 		
 		// get membership ids and org contact ids.
 		$mem_ids = array();
 		$org_ids = array();
-		$api_result = civicrm_api3('MembershipType', 'get', array(
-				'sequential' => 1,
-				'is_active' => 1,
-				'domain_id' =>  $cur_domain_id ,
-				'options' => array('sort' => "name"),
-		));
-		
-		if( $api_result['is_error'] == 0 ){
-			$tmp_api_values = $api_result['values'];
-			foreach($tmp_api_values as $cur){
-		
-				$tmp_id = $cur['id'];
-				$mem_ids[$tmp_id] = $cur['name'];
-					
-				$org_id = $cur['member_of_contact_id'];
-				// get display name of org
-				$result = civicrm_api3('Contact', 'getsingle', array(
-						'sequential' => 1,
-						'id' => $org_id ,
-				));
-				$org_ids[$org_id] = $result['display_name'];
-		
-					
-			}
-		
-		}	
+			
+		require_once( 'CRM/Familyfriendly/Form/FamilyFriendlyFilterHelper.php');
+		FamilyFriendlyFilterHelper::fillMembershipTypeArrays( $mem_ids,  $org_ids);
 
 	 $gender_options =  CRM_Contact_BAO_Contact::buildOptions('gender_id');
 	  
